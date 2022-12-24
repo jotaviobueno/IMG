d35c911d5b2e0a832e813a2b878ac7d82f63f606
unknown <joseotaviocbueno@gmail.com>
2022-12-24 15:05:18 -0300

    adicionado os middlewares

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,10 +1,16 @@
 import {Router} from 'express';
 
+// Controller's
 import CreateUserController from '../Module/User/app/useCases/createUser/CreateUserController.js';
-import CreateSessionController from '../Module/User/app/useCases/createSession/CreateSessionController.js';
+import ShowProfileController from '../Module/User/app/useCases/showProfile/ShowProfileController.js';
+
+// Middlewares
+import ValidateSession from '../Module/User/app/Middleware/ValidateSession.js';
+import ValidateUser from '../Module/User/app/Middleware/ValidateUser.js';
 
 const userRoutes = Router();
 
 userRoutes.post("/", CreateUserController.handle );
+userRoutes.get("/", ValidateSession, ValidateUser, ShowProfileController.handle );
 
 export {userRoutes};
