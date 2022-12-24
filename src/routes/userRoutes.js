67d7cfd79c02a9c546608fe7fa4c9504995d1ba1
unknown <joseotaviocbueno@gmail.com>
2022-12-24 16:31:38 -0300

    adicionado a importação do update request

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -11,12 +11,13 @@ import ValidateSession from '../Module/User/app/Middleware/ValidateSession.js';
 import ValidateUser from '../Module/User/app/Middleware/ValidateUser.js';
 
 import UserRequest from '../Module/User/app/Request/UserRequest.js';
+import UpdateRequest from '../Module/User/app/Request/UpdateRequest.js'
 
 const userRoutes = Router();
 
-userRoutes.post("/", UserRequest.storage, CreateUserController.handle );
-userRoutes.get("/", UserRequest.profile, ValidateSession, ValidateUser, ShowProfileController.handle );
-userRoutes.get("/:username", UserRequest.outherProfile, ValidateSession, ShowOutherProfileController.handle );
-userRoutes.put("/update/username", ValidateSession, ValidateUser, UpdateUsernameController.handle );
+userRoutes.post("/", UserRequest.storage, CreateUserController.handle);
+userRoutes.get("/", UserRequest.profile, ValidateSession, ValidateUser, ShowProfileController.handle);
+userRoutes.get("/:username", UserRequest.outherProfile, ValidateSession, ShowOutherProfileController.handle);
+userRoutes.put("/update/username", UpdateRequest.updateUsername, ValidateSession, ValidateUser, UpdateUsernameController.handle);
 
 export {userRoutes};
