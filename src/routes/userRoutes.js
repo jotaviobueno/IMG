a7418864cbdfae3873bdbf41e0a1ff872b34145b
unknown <joseotaviocbueno@gmail.com>
2022-12-24 15:42:26 -0300

    adicionado a importação do updateUsernameControlelr

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,6 +3,7 @@ import {Router} from 'express';
 // Controller's
 import CreateUserController from '../Module/User/app/useCases/createUser/CreateUserController.js';
 import ShowProfileController from '../Module/User/app/useCases/showProfile/ShowProfileController.js';
+import UpdateUsernameController from '../Module/User/app/useCases/updateUsername/UpdateUsernameController.js';
 
 // Middlewares
 import ValidateSession from '../Module/User/app/Middleware/ValidateSession.js';
@@ -12,5 +13,6 @@ const userRoutes = Router();
 
 userRoutes.post("/", CreateUserController.handle );
 userRoutes.get("/", ValidateSession, ValidateUser, ShowProfileController.handle );
+userRoutes.put("/update/username", ValidateSession, ValidateUser, UpdateUsernameController.handle );
 
 export {userRoutes};
