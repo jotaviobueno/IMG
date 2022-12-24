05b0d72ae7675ea7c68f16bd4f960742afe7da15
unknown <joseotaviocbueno@gmail.com>
2022-12-24 14:53:55 -0300

    nova configuração do mongoose

--- src/database/MongoDB/connection.js
+++ src/database/MongoDB/connection.js
@@ -1,13 +1,15 @@
 import mongoose from "mongoose";
 import {storage} from "../../config/storage.js";
 
-const connection = () => {
+export default function connectionToMongosoe() {
 	mongoose.connect(storage.mongoURI, {
 		useNewUrlParser: true,
-		useUnifiedTopology: true
-	  }).then(() => {
-		console.log("Database connection established.");
+		useUnifiedTopology: true,
+		autoIndex: false
 	});
-};
 
-export default connection();
+	const db = mongoose.connection;
+
+	db.on("error", (error) => console.error(error));
+	db.once("open", () => console.log("Database connection established."))
+};
