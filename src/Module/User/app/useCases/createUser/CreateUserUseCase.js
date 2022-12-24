0007a2f4d1610a6dd4c7db3674195a3d3be773aa
unknown <joseotaviocbueno@gmail.com>
2022-12-24 15:02:33 -0300

    adicionado o UserDTO no return

--- src/Module/User/app/useCases/createUser/CreateUserUseCase.js
+++ src/Module/User/app/useCases/createUser/CreateUserUseCase.js
@@ -1,5 +1,7 @@
 import UserRepository from "../../Repositories/UserRepository.js";
 
+import UserDTO from "../../DTO/Response/UserDTO.js";
+
 class CreateUserUseCase {
     
     _userRepository;
@@ -18,20 +20,9 @@ class CreateUserUseCase {
         const user = await this._userRepository.createUser(full_name, username, email, password, birth_date, avatar_url);
 
         if ( user )
-            return {status: 201, message: { user: {
-                full_name: user.full_name,
-                username: user.username,
-                email: user.email,
-                avatar_url: user.avatar_url,
-                birth_date: user.birth_date,
-                permissions: user.permissions,
-                created_at: user.created_at,
-                updated_at: user.updated_at,
-                deleted_at: user.deleted_at,
-            }
-        }};
+            return {status: 201, message: { user: UserDTO(user) }};
 
-        return {status: 500, message: { error: "error" }};
+        return {status: 500, message: { error: "invalid request, try again" }};
     }
 }
 
