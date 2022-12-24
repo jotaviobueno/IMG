53ecf93d414c1b05d67a42796204522b448de579
unknown <joseotaviocbueno@gmail.com>
2022-12-24 14:54:36 -0300

    adicionado Repsonse DTO

---
+++ src/Module/User/app/DTO/Response/UserDTO.js
@@ -0,0 +1,12 @@
+export default function UserDTO (user) {
+    return {
+        full_name: user.full_name,
+        username: user.username,
+        email: user.email,
+        avatar_url: user.avatar_url,
+        birth_date: user.birth_date,
+        created_at: user.created_at,
+        updated_at: user.updated_at,
+        deleted_at: user.deleted_at,
+    }
+}
