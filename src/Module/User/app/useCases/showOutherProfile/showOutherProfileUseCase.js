36d7b3ed2e261a7f190b9e81da99ed7d971dac2a
unknown <joseotaviocbueno@gmail.com>
2022-12-26 16:02:07 -0300

    removida linhas inúteis

--- src/Module/User/app/useCases/showOutherProfile/showOutherProfileUseCase.js
+++ src/Module/User/app/useCases/showOutherProfile/showOutherProfileUseCase.js
@@ -18,9 +18,7 @@ class ShowOutherProfileUseCase {
             return {status: 404, message: { error: "username not found" }};
         else
             return {status: 200, message: { user: UserDTO(user) }};
-
     }
-
 }
 
 export default ShowOutherProfileUseCase
