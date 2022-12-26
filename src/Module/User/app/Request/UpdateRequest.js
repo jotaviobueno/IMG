0159f9b5f5096fb69c4a00459c4e748d4223d59f
unknown <joseotaviocbueno@gmail.com>
2022-12-26 15:26:47 -0300

    adicionado o request validator

--- src/Module/User/app/Request/UpdateRequest.js
+++ src/Module/User/app/Request/UpdateRequest.js
@@ -22,6 +22,27 @@ class UpdateRequest {
 
 		await next();
 	}
+
+	async updateFullname ( req, res, next ) {
+
+		const headersValidator = yup.object().shape({
+			session_id: yup.string().required()
+		});
+
+		const bodyValidator = yup.object().shape({
+			full_name: yup.string().max(50).required(),
+		});
+
+		try {
+			await headersValidator.validate(req.headers);
+			await bodyValidator.validate(req.body);
+
+		} catch (err) {
+			return res.status(400).json({errors: err.errors});
+		}
+
+		await next();
+	}
 }
 
 export default new UpdateRequest;
