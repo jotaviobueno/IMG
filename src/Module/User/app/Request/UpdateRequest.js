3d803f35a564af802cb64b93cd9b724c82439748
unknown <joseotaviocbueno@gmail.com>
2022-12-27 15:21:50 -0300

    adicionado o request validator do update bith date

--- src/Module/User/app/Request/UpdateRequest.js
+++ src/Module/User/app/Request/UpdateRequest.js
@@ -43,6 +43,27 @@ class UpdateRequest {
 
 		await next();
 	}
+
+	async updateBirthdate ( req, res, next ) {
+
+		const headersValidator = yup.object().shape({
+			session_id: yup.string().required()
+		});
+
+		const bodyValidator = yup.object().shape({
+			birth_date: yup.date().required(),
+		});
+
+		try {
+			await headersValidator.validate(req.headers);
+			await bodyValidator.validate(req.body);
+
+		} catch (err) {
+			return res.status(400).json({errors: err.errors});
+		}
+
+		await next();
+	}
 }
 
 export default new UpdateRequest;
