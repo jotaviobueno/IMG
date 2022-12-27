e20592c4d50f3e6eaa6379e8f6c58a0e1ef7f4a6
unknown <joseotaviocbueno@gmail.com>
2022-12-27 15:08:15 -0300

    adicionado update birth date

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,8 +3,9 @@ import {Router} from 'express';
 // Controller's
 import CreateUserController from '../Module/User/app/useCases/createUser/CreateUserController.js';
 import ShowProfileController from '../Module/User/app/useCases/showProfile/ShowProfileController.js';
-import UpdateUsernameController from '../Module/User/app/useCases/updateUsername/UpdateUsernameController.js';
 import ShowOutherProfileController from '../Module/User/app/useCases/showOutherProfile/showOutherProfileController.js';
+import UpdateUsernameController from '../Module/User/app/useCases/updateUsername/UpdateUsernameController.js';
+import updateBirthDateController from '../Module/User/app/useCases/updateBirthDate/updateBirthDateController.js';
 
 // Middlewares
 import ValidateSession from '../Module/User/app/Middleware/ValidateSession.js';
@@ -20,5 +21,6 @@ userRoutes.post("/", UserRequest.storage, CreateUserController.handle);
 userRoutes.get("/", UserRequest.profile, ValidateSession, ValidateUser, ShowProfileController.handle);
 userRoutes.get("/:username", UserRequest.outherProfile, ValidateSession, ShowOutherProfileController.handle);
 userRoutes.put("/update/username", UpdateRequest.updateUsername, ValidateSession, ValidateUser, UpdateUsernameController.handle);
+userRoutes.put("/update/birthdate", UpdateRequest.updateBirthdate, ValidateSession, ValidateUser, updateBirthDateController.handle);
 
 export {userRoutes};
