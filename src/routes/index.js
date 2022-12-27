5b4c5db507a29a49144458d9cf0cae9145aef135
unknown <joseotaviocbueno@gmail.com>
2022-12-27 18:28:40 -0300

    nova importação

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,8 +4,10 @@ const routes = Router();
 
 import {sessionRoutes} from './sessionRoutes.js';
 import {userRoutes} from './userRoutes.js';
+import { adminRoutes } from './adminRoutes.js';
 
 routes.use("/user", userRoutes);
 routes.use("/session", sessionRoutes);
+routes.use("/admin", adminRoutes);
 
 export default routes;
