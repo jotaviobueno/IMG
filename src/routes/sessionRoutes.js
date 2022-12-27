e7ac3a1f61d1104fda6c4e4c37d3d145b72b979d
unknown <joseotaviocbueno@gmail.com>
2022-12-26 22:03:47 -0300

    adicionado... desconectar uma sessão, é desconectar todas as sessões

--- src/routes/sessionRoutes.js
+++ src/routes/sessionRoutes.js
@@ -2,6 +2,8 @@ import {Router} from 'express';
 
 import CreateSessionController from '../Module/User/app/useCases/createSession/CreateSessionController.js';
 import GetAllSessionController from '../Module/User/app/useCases/getAllSession/getAllSessionController.js';
+import DisconnectSessionController from '../Module/User/app/useCases/disconnectSession/disconnectSessionController.js';
+import DisconnectAllSessionController from '../Module/User/app/useCases/disconnectAllSession/disconnectAllSessionController.js';
 
 // Middlewares
 import ValidateSession from '../Module/User/app/Middleware/ValidateSession.js';
@@ -11,5 +13,7 @@ const sessionRoutes = Router();
 
 sessionRoutes.post("/", CreateSessionController.handle );
 sessionRoutes.get("/", ValidateSession, ValidateUser, GetAllSessionController.handle );
+sessionRoutes.post("/disconnect", ValidateSession, ValidateUser, DisconnectSessionController.handle );
+sessionRoutes.post("/many-disconnect", ValidateSession, ValidateUser, DisconnectAllSessionController.handle );
 
 export {sessionRoutes};
